43c84cfcb48de86a80845a9ef8a18e7108708611
Lillian <lilynoordhoek@gmail.com>
2024-06-12 11:16:02 -0500

    fixed getSeries()

--- mad-datta/src/analyzers/helpers/getSeries.js
+++ mad-datta/src/analyzers/helpers/getSeries.js
@@ -1,8 +1,20 @@
 import { colors } from "../../../config";
-export default function getSeries(data) {
+export default function getSeries(column) {
 	let colorIndex = 0;
-	return Object.keys(data).map((m) => {
-		colorIndex >= colors.length ? (colorIndex = 0) : colorIndex++;
-		return { name: m, color: colors[colorIndex] };
-	});
+	// if the series are already defined as options, returns a list of them in {name,color} format
+	// else, reduces to get the options and then returns the series list.
+	return column.options
+		? column.options.map((m) => {
+				colorIndex >= colors.length ? (colorIndex = 0) : colorIndex++;
+				return { name: m, color: colors[colorIndex] };
+		  })
+		: column.data
+				.reduce((acc, data) => {
+					!acc.includes(data) ? acc.push(data) : null;
+					return acc;
+				}, [])
+				.map((m) => {
+					colorIndex >= colors.length ? (colorIndex = 0) : colorIndex++;
+					return { name: m, color: colors[colorIndex] };
+				});
 }
